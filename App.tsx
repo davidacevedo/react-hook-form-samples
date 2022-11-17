136892e445938c02bbf9daab0d22dc6ed716bd3c
davidacevedo <davidacevedo@users.noreply.github.com>
2022-11-17 10:43:58 -0800

    fix small thing

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import { Button, Modal, ModalHeader, ModalBody } from '@appfolio/react-gears';
-import { useForm, UseFormRegisterReturn } from 'react-hook-form';
 import SimpleForm, { OldForm } from './components/SimpleForm';
 import ValidationExamples, { OldFormValidationsExample } from './components/ValidationExamples';
 import ComplicatedForm from './components/ComplicatedForm';
-
-export const refToInnerRef = ({ ref, ...rest }: UseFormRegisterReturn) => {
-  return { ...rest, innerRef: ref };
-};
 
 export default function NewUser() {
   const [page, setPage] = React.useState<'simple' | 'validation' | 'complicated'>('simple');
